1f901b750ec49864cc7fbc2217a907ef7a2a37bc
MuhammadOusman <ousmansohail786@gmail.com>
2025-06-25 03:20:57 +0500

    react.js assignment 1 complete

---
+++ react/assi_01/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="main-heading">
+        Hello World & Hello React !!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
